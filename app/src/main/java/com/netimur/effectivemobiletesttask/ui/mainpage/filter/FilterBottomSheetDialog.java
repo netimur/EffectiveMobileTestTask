f68a964c6b284be607133b054d7948566834f1ff
netimur <netimur@internet.ru>
2022-11-26 13:45:29 +0600

    Code refactored

--- app/src/main/java/com/netimur/effectivemobiletesttask/ui/mainpage/filter/FilterBottomSheetDialog.java
+++ app/src/main/java/com/netimur/effectivemobiletesttask/ui/mainpage/filter/FilterBottomSheetDialog.java
@@ -24,38 +24,25 @@ final public class FilterBottomSheetDialog extends BottomSheetDialogFragment {
         binding = FilterBottomSheetDialogBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         specifySpinners();
-        binding.closeDialogButton.setOnClickListener(v -> {
-            this.dismiss();
-        });
+        binding.closeDialogButton.setOnClickListener(v -> this.dismiss());
         return view;
     }
 
     private void specifySpinners() {
         Spinner brandSpinner = binding.brandSpinner;
-// Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(),
                 R.array.brands_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         brandSpinner.setAdapter(adapter);
-
         Spinner priceSpinner = binding.priceSpinner;
-// Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> priceAdapter = ArrayAdapter.createFromResource(this.getContext(),
                 R.array.prices_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         priceSpinner.setAdapter(priceAdapter);
-
         Spinner sizeSpinner = binding.sizeSpinner;
-// Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> sizeAdapter = ArrayAdapter.createFromResource(this.getContext(),
                 R.array.sizes_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         sizeSpinner.setAdapter(sizeAdapter);
     }
 }
